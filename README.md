5e8e79da3caca9e08c0e2b02ac019d1efa38d135
dblock <dblock@dblock.org>
2022-01-02 21:11:24 -0500

    Yoy vs. grant date.

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ Last 10 years of AMZN stock [raw data](data/amzn.csv) from [IEX](https://iexclou
 
 #### YoY Averages on Date of Vesting
 
-Average yoy, compounded stock growth, on vesting date, with AMZN schedule via [yoy.rb](yoy.rb), see [raw data](data/yoy.csv).
+Average yoy compounded stock growth and yoy non-componded stock growth, on vesting date, with AMZN schedule via [yoy.rb](yoy.rb). See [raw data](data/yoy.csv). 
 
-![](images/yoy.png)
+Every blue point in this graph shows the stock price growth in % compared to the previous year, and in orange the stock price growth in % compared to one's grant date, at the time of stock vesting averaged for the year when the stock vested.
 
+![](images/growth.png)
--- yoy.rb
+++ yoy.rb
@@ -4,12 +4,24 @@ require 'date'
 require 'active_support'
 require 'active_support/core_ext'
 
+class Array
+    def sum
+        inject(0.0) { |result, el| result + el }
+    end
+  
+    def mean 
+        result = sum / size if size
+        return result&.to_i unless result.nan?
+        nil
+    end
+end
+
 class MarketData
     VESTING_MONTHS = [12, 18, 24, 30, 36, 42, 48]
 
     def initialize
         @data = {}
-        CSV.foreach('amzn.csv', headers: true) do |row|
+        CSV.foreach('data/amzn.csv', headers: true) do |row|
             row['u_close'] = row['u_close'].to_f
             @data[row['date']] = row
         end
@@ -34,13 +46,15 @@ class MarketData
         year_cliff = dt
         grant_data = data_at(dt)
         u_close_base = grant_data['u_close']
+        u_grant_base = grant_data['u_close']
         VESTING_MONTHS.each do |m|
             data = data_at(dt + m.months)
             next unless data
             results[m] = {
                 close: data['u_close'],
                 total_growth: data['u_close'] - grant_data['u_close'],
-                yoy_growth: data['u_close'] * 100 / u_close_base - 100
+                yoy_growth: data['u_close'] * 100 / u_close_base - 100,
+                grant_growth: data['u_close'] * 100 / u_grant_base - 100
             }
             # reset base to years only 
             u_close_base = results[m][:close] if [12, 24, 36, 48].include?(m)
@@ -49,22 +63,33 @@ class MarketData
     end
 end
 
-column_names = ['date', 'avg'] + MarketData::VESTING_MONTHS.map(&:to_s)
+puts "Loading data ..."
+data = MarketData.new
+
+puts "Generating averages ..."
+column_names = ['grant date', 'average yoy', 'average since grant']
 s = CSV.generate do |csv|
     csv << column_names
-
-    data = MarketData.new
     dt = data.start_at.to_date
     loop do
-        next unless data.data_at(dt)
-        vests = data.vesting_prices(dt)
-        yoy = MarketData::VESTING_MONTHS.map do |m|
-            vests.key?(m) ? vests[m][:yoy_growth].to_i : nil
-        end.compact
-        csv << [dt.to_s, (yoy.sum(0.0) / yoy.size).to_i] + yoy if yoy.any?
+        puts dt.year if dt.day == 31 && dt.month == 12
+        if data.data_at(dt)
+            vests = data.vesting_prices(dt)
+            # average yoy
+            yoy = MarketData::VESTING_MONTHS.map do |m|
+                (vests[m] || {})[:yoy_growth]
+            end.compact.mean
+            # average since grant
+            grant = MarketData::VESTING_MONTHS.map do |m|
+                (vests[m] || {})[:grant_growth]
+            end.compact.mean
+            csv << [dt.to_s, yoy, grant]
+        end
         dt = dt + 1.day
         break if dt >= Time.now - 6.months
     end
 end
 
+puts "Saving data ..."
 File.write('data/yoy.csv', s)
+
